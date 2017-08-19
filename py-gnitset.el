c53b0a0c4bbe1930d19a433e4c7f33bbbc80b7b3
Brandon W Maister <quodlibetor@gmail.com>
2017-08-19 18:04:41 -0500

    re-license (correctly) under GPLv3+

--- py-gnitset.el
+++ py-gnitset.el
@@ -4,7 +4,21 @@
 ;; Copyright (C) 2014 Brandon W Maister
 ;; Author: Brandon W Maister <quodlibetor@gmail.com>
 ;; URL: https://www.github.com/quodlibetor/py-gnitset
-;; License: GPLv2+ http://www.gnu.org/licenses/gpl-2.0.html
+;;
+;; License: GNU GPL version 3, or (at your option) any later version
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;;
